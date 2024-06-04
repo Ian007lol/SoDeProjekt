5e47f9f4965baa71a779813fa4eef0de6049e7c6
Ian007lol <zamorabellaian@gmail.com>
2024-06-04 19:15:35 +0200

    Optimization

--- Test.py
+++ Test.py
@@ -4,35 +4,47 @@ import os
 from PIL import Image
 import time
 import cProfile
+filnam = []
 
-def add_two_numbers():
-
-    return
 
 def load_images_from_folder(folder):
     for filename in os.listdir(folder):
         if filename.endswith(".bmp"): # add file types as needed
-            with Image.open(os.path.join(folder, filename)) as img:
-                img_np = np.array(img)  # convert image to numpy array
-                img_bgr = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)  # convert image to BGR
-
-                if img_bgr[0][0].tolist() != [154,203,102] and img_bgr[0][0].tolist() != [150,190,110]:
-                    if img_bgr.shape == (1920,1080,3):
-                        if  (img_bgr[1800][450].tolist() == [52,113,0] 
-                            or img_bgr[1800][325].tolist() == [52,113,0] 
-                            or img_bgr[1800][575].tolist() == [52,113,0] 
-                            or img_bgr[1800][700].tolist() == [52,113,0] 
-                            or img_bgr[1800][825].tolist() == [52,113,0] 
-                            or img_bgr[1800][950].tolist() == [52,113,0]
-                            or img_bgr[1800][450].tolist() == [38,12,145] 
-                            or img_bgr[1800][325].tolist() == [38,12,145] 
-                            or img_bgr[1800][575].tolist() == [38,12,145] 
-                            or img_bgr[1800][700].tolist() == [38,12,145] 
-                            or img_bgr[1800][825].tolist() == [38,12,145]  
-                            or img_bgr[1800][950].tolist() == [38,12,145]):
-                                yield img_bgr
-                    else:
-                        yield img_bgr
+            if os.path.getsize(folder+"/"+filename) <= 330544 and os.path.getsize(folder+"/"+filename)>=329359 or "perl" in filename:
+                continue  
+            else:
+                with Image.open(os.path.join(folder, filename)) as img:
+                    img_np = np.array(img)  # convert image to numpy array
+                    img_bgr = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)  # convert image to BGR
+                    if img_bgr[0][0].tolist() != [154,203,102] and img_bgr[0][0].tolist() != [150,190,110]:
+                            if  (img_bgr[1800][450].tolist() == [52,113,0] 
+                                or img_bgr[1800][325].tolist() == [52,113,0] 
+                                or img_bgr[1800][575].tolist() == [52,113,0] 
+                                or img_bgr[1800][700].tolist() == [52,113,0] 
+                                or img_bgr[1800][825].tolist() == [52,113,0] 
+                                or img_bgr[1800][950].tolist() == [52,113,0]
+                                or img_bgr[1800][450].tolist() == [38,12,145] 
+                                or img_bgr[1800][325].tolist() == [38,12,145] 
+                                or img_bgr[1800][575].tolist() == [38,12,145] 
+                                or img_bgr[1800][700].tolist() == [38,12,145] 
+                                or img_bgr[1800][825].tolist() == [38,12,145]  
+                                or img_bgr[1800][950].tolist() == [38,12,145]
+                                or img_bgr[1800][140].tolist() == [52,113,0] 
+                                or img_bgr[1800][260].tolist() == [52,113,0] 
+                                or img_bgr[1800][380].tolist() == [52,113,0] 
+                                or img_bgr[1800][500].tolist() == [52,113,0] 
+                                or img_bgr[1800][620].tolist() == [52,113,0] 
+                                or img_bgr[1800][740].tolist() == [52,113,0]
+                                or img_bgr[1800][140].tolist() == [38,12,145] 
+                                or img_bgr[1800][260].tolist() == [38,12,145] 
+                                or img_bgr[1800][380].tolist() == [38,12,145] 
+                                or img_bgr[1800][500].tolist() == [38,12,145] 
+                                or img_bgr[1800][620].tolist() == [38,12,145]  
+                                or img_bgr[1800][740].tolist() == [38,12,145]):
+                                    filnam.append(filename)
+                                    yield img_bgr
+                    
+                        
 
 
 def sample_size(folder):
@@ -45,7 +57,7 @@ def sample_size(folder):
 
 #load folder name
 folder = "smmiMai"
-img_test = cv2.imread("smmiMai_Test\screen-left-2022-05-29-17-10.bmp")
+save_folder= "sorted"
 sampleSize = sample_size(folder)
 counter = 0
 image = []
@@ -56,6 +68,7 @@ start = time.time()
 images = load_images_from_folder(folder)
 for img in images:
     counter = counter +1
+    cv2.imwrite(save_folder+"/"+filnam[counter-1], img)
     image.append(img)
 
 #stop the timer
